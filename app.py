2e89ca96a40ae0bd530398ed87032466716a9465
David Sanchez <david.sanchez@karmapulse.com>
2019-12-16 17:45:20 -0600

    Add methods to path and invocation counter

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import request
+from flask import Flask, jsonify
+from multiprocessing import Value
+
+counter = Value('i', 0)
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return "Hello World!!!"
+    if request.method == 'GET':
+        return "Hello World!!!"
+    if request.method == 'POST':
+        counter.value += 1
+        return "Hello World!!!"
+
+@app.route("/count")
+def count():
+    return "Conteo de invocaciones: " + str(counter.value)
 
 
 if __name__ == "__main__":
